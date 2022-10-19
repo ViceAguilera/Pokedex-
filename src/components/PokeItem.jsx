0d83f1a013ea073494b4a961e2ecb2340a3f52b9
Vicente Aguilera <vicente.aguilera2001@alumnos.ubiobio.cl>
2022-10-19 10:47:04 -0300

    Avance 19-10

--- src/components/PokeItem.jsx
+++ src/components/PokeItem.jsx
@@ -1,12 +1,19 @@
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import React from 'react';
 
-const PokeItem = ({pokemon}) => {
-    return (<div>{pokemon.name}</div>)
+const PokeItem = ({pokemon, estilo = null, funcion = null}) => {
+    //return (<div>{pokemon.name}</div>)
+    return(
+      <Card sx={{estilo}}>
+        <CardContent>{pokemon.name}</CardContent>
+        <CardActions>
+            {funcion && <Button onClick={() => funcion(pokemon)}>Accion</Button>}
+        </CardActions>
+     </Card>
+    )
 }
 
 export default PokeItem
--- src/components/PokeList.jsx
+++ src/components/PokeList.jsx
@@ -3,16 +3,37 @@ import { useEffect, useState } from "react";
 import Box from '@mui/material/Box';
 import PokeItem from "./PokeItem";
 import {getPokemon} from "../utils/getPokemons";
-import { Grid, TextField } from "@mui/material";
+import { Grid, TextField, Card, CardContent  } from "@mui/material";
+import { Stack } from "@mui/system";
 
 const PokeList = () => {
     const [pokemons, setPokemons] = useState([]);
     const [finder, setFinder] = useState("");
-    const [listaAux, setlistaAux] = useState([]); //se declaran los estados
+    const [listaAux, setListaAux] = useState([]);
+    const [errors, setErrors] = useState(false);
+    const [listadoSelected, setListadoSelected] = useState([]);//se declaran los estados
 
     const handleInputChange = (event) => { //
       setFinder(event.target.value)
     };
+
+     const stack = itemExterno => {
+        setListadoSelected ((listadoSelected) => [...listadoSelected, itemExterno]);//se agrega el item al listado
+       
+        let result = pokemons.filter((item)=>(item.name !== itemExterno.name));
+        setPokemons(result);
+
+        let resultClon = listaAux.filter((item)=>(item.name !== itemExterno.name));
+        setListaAux(resultClon);
+    };
+
+    // const unStack = itemExterno => {
+    //     setPokemons ((pokemons) => [...pokemons, itemExterno]);//se agrega el item al listado
+    //     setListaAux ((listaAux) => [...listaAux, itemExterno]);//se agrega el item al listado
+
+    //     let result = listadoSelected.filter((item)=>(item.name !== itemExterno.name));
+    //     setListadoSelected(result);
+    // };
 
     useEffect(() => {
         getPokemon().then((data) => setPokemons(data));
@@ -22,30 +43,55 @@ const PokeList = () => {
       let aBuscar = finder.toString().trim()
       if(aBuscar !== ""){
         const result = pokemons.filter((item)=>(
-          item.name.toString().includes(aBuscar)
-        ))
-        setlistaAux(result)
+          item.name.toString().includes(aBuscar)));
+        if(result.length !== 0){
+          errors && setErrors(false)
+          setListaAux(result);
+        }else{
+          setListaAux([]);
+          setErrors(true);
+        }
       }
       console.log(finder)
     },[finder]);
 
+    let estilo = { backgroundColor: "blue"};
+
     
   return <>
-  <TextField id="outlined-basic" label="Outlined" variant="outlined" value={finder} placeholder={"Busca tu pokemon"} onChange={handleInputChange} />
-  <br />
-  <Grid container spacing={2}>
-    <Grid item md={6}>
-      {pokemons.map((element, index) => (
-        <PokeItem pokemon = {element} key={element.name}></PokeItem>
-      ))}
-    </Grid>
-    <Grid item md={6}>
-        {finder &&
-          listaAux.map((element, index) => (
-            <PokeItem pokemon = {element} key={element.name} ></PokeItem>
-          ))}
-    </Grid>
-  </Grid>
+    <Card>
+      <CardContent>
+        <TextField 
+          id="outlined-basic" 
+          error={errors}
+          helperText={errors ? "hay error" : null} 
+          label="Outlined" 
+          variant="outlined" 
+          value={finder} 
+          placeholder={"Busca tu pokemon"} 
+          onChange={handleInputChange} />
+        <br />
+        <Grid container spacing={2}>
+          <Grid item md={6}>
+            {pokemons.map((element, index) => (
+              <PokeItem pokemon = {element} key={element.name}></PokeItem>
+            ))}
+          </Grid>
+          <Grid item md={3}>
+              {finder &&
+                listaAux.map((element, index) => (
+                  <PokeItem pokemon = {element} key={element.name} funcion = {stack} estilo = {estilo} ></PokeItem>
+                ))}
+          </Grid>
+          <Grid item md = {3}>
+                {finder &&
+                  listadoSelected.map((element, index) => (
+                    <PokeItem pokemon = {element} key={element.name} funcion = {stack} estilo = {estilo} ></PokeItem>    
+                  ))}
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
   </>
 };
 
